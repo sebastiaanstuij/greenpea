ac55aac6a7d7656c825dbe339418cee2833d0f13
Sebastiaan <sebastiaan@Sebastiaans-MacBook-Pro.local>
2017-01-06 15:31:08 +0100

    implemented basic thingsnetwork functionality

--- webserver/app.js
+++ webserver/app.js
@@ -1,7 +1,54 @@
-var http = require('http');
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Groene stroom, direct frituren!\n");
+// var http = require('http');
+var ttn = require('ttn');
+
+var region = 'eu';
+var appId = 'arduinonodetest';
+var accessKey = 'ttn-account-v2.HWSBtS5jSvN0Lb0fvltzrLFd7zPuRKWP5ay73kmHjx4';
+
+var client = new ttn.Client(region, appId, accessKey);
+
+client.on('connect', function(connack) {
+  console.log('[DEBUG]', 'Connect:', connack);
 });
-server.listen(80);
-console.log("Server running at http://192.168.1.104:80/");
+
+client.on('error', function(err) {
+  console.error('[ERROR]', err.message);
+});
+
+client.on('activation', function(deviceId, data) {
+  console.log('[INFO] ', 'Activation:', deviceId, JSON.stringify(data, null, 2));
+});
+
+client.on('device', null, 'down/scheduled', function(deviceId, data) {
+  console.log('[INFO] ', 'Scheduled:', deviceId, JSON.stringify(data, null, 2));
+});
+
+client.on('message', function(deviceId, data) {
+  console.info('[INFO] ', 'Message:', deviceId, JSON.stringify(data, null, 2));
+});
+
+client.on('message', null, 'led', function(deviceId, led) {
+
+  // Toggle the LED
+  var payload = {
+    led: !led
+  };
+
+  // If you don't have an encoder payload function:
+  // var payload = [led ? 0 : 1];
+
+  console.log('[DEBUG]', 'Sending:', JSON.stringify(payload));
+  client.send(deviceId, payload);
+});
+
+
+// var server = http.createServer(function (request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.end("Groene stroom, direct frituren!\n");
+// });
+// server.listen(80);
+// console.log("Server running at http://192.168.1.104:80/");
+
+
+
+
